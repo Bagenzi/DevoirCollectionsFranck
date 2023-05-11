defe6a07ebc02547db642c0959d642dd504e4b0a
Bagenzi <bagefranck@gmail.com>
2023-05-11 19:46:31 +0200

    Affichage de la liste

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-# Créer une liste de 10 éléments de type chaîne de caractères
+# Création une liste de 10 éléments de type chaîne de caractères
+#_______________________________________________________________
 liste = ["bagenzi", "ciza", "muco", "nkingi", "keza", "gakiza", "iranzi", "ishimwe", "ndaganje", "irankunda"]
+
+# Affichage des éléments de la liste
+#-----------------------------------
+print("Liste initiale : ", liste)
